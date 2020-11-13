ed4ce6498e6104eb91c51108baf00153149d26b2
swapnil0709 <swapnilsinha20@gmail.com>
2020-11-13 23:57:25 +0530

    Blog Website v1.0

---
+++ app.js
@@ -0,0 +1,89 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+const homeStartingContent =
+  "Welcome to the Daily Journal. The best and most useful articles you will find here. With over 500 Articles published daily from professional writers, this is indeed the blog site you were looking for, lets get started by signing up to our news letter click ";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogsDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const blogsSchema = {
+  title: String,
+  content: String,
+};
+
+const Blog = mongoose.model("Blog", blogsSchema);
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function (req, res) {
+  Blog.find({}, (err, foundBlogs) => {
+    if (!err) {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: foundBlogs,
+      });
+    }
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const myBlog = new Blog({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+  myBlog.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  Blog.find({}, (err, foundBlogs) => {
+    foundBlogs.forEach(function (post) {
+      const storedTitle = _.lowerCase(post.title);
+
+      if (storedTitle === requestedTitle) {
+        res.render("post", {
+          title: post.title,
+          content: post.content,
+        });
+      }
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
